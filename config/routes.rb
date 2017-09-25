d08f4e82fd0ea357f755dd5beb839f36befe4461
Sara Hopper <sara.hopper@gmail.com>
2017-09-25 15:49:09 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
-  resources :products, only: [:show, :index, :new, :create]
+  resources :product
 end
